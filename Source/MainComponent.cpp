9542594feea19480f4fd7e1992626dba86644536
y_S <yevgeniy.sverdlik@gmail.com>
2023-09-19 07:13:19 -0700

    Deleted toggleButton

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,7 +3,6 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    addAndMakeVisible(toggleButton);
     setSize (600, 400);
 }
 
@@ -27,6 +26,5 @@ void MainComponent::resized()
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
-    toggleButton.setBounds(200, 100, 30, 30);
     
 }
